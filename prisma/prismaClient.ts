dba6a12496352d8594b047dfd6b7901e49cca084
KIRIN MURPHY <kirinmurphy@gmail.com>
2024-07-11 17:28:09 -0500

    netlify test

--- prisma/prismaClient.ts
+++ prisma/prismaClient.ts
@@ -1,5 +1,16 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  // Ensure the PrismaClient is reused during development to avoid exhausting your database connection limit.
+  // Using 'globalThis' to access the global object and declare a type for 'prisma'
+  if (!(globalThis as any).prisma) {
+    (globalThis as any).prisma = new PrismaClient();
+  }
+  prisma = (globalThis as any).prisma;
+}
 
 export default prisma;
