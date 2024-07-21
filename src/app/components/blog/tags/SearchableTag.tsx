7e27274e82eb7e080382e25adfc388bc1d56369e
codethings <kirinmurphy@gmail.com>
2024-07-20 19:42:05 -0500

    client side filtering (#2)

--- src/app/components/blog/tags/SearchableTag.tsx
+++ src/app/components/blog/tags/SearchableTag.tsx
@@ -1,16 +1,28 @@
 "use client" 
 
- import { TagWithCount } from "@/src/lib/fetchTagsFacet";
+import { TagWithCount } from "@/src/lib/fetchTagsFacet";
 import { Button, ButtonType } from "../../widgets/Button";
+import { useRouter } from "next/navigation";
 
-export function SearchableTag({ tag }: { tag: TagWithCount }) {
-  
+interface Props {
+  tag: TagWithCount;
+  currentTagName: string;
+}
+
+export function SearchableTag({ tag, currentTagName }: Props) {
+  const router = useRouter();
+    
+  const isCurrentTag = tag.name === currentTagName;
+
   const handleTagClick = () => {
-    window.location.href = `/?tag=${tag.name}`;
+    const newPath = isCurrentTag ? '/' : `/?tag=${tag.name}`; 
+    router.push(newPath, { scroll: false });
   };
 
+  const buttonState = isCurrentTag ? ButtonType.TagActive : ButtonType.Tag;
+
   return (
-    <Button type={ButtonType.Tag} onClick={handleTagClick}>
+    <Button type={buttonState} onClick={handleTagClick}>
       <span className="text-lg">{tag.readableName}</span>
       <span className="text-sm">{tag.count}</span>
     </Button>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,13 @@ import { fetchBlogPosts } from '@/src/lib/fetchBlogPosts';
 import { BlogListItem } from './components/blog/BlogListItem/index';
 import { SearchableTag } from './components/blog/tags/SearchableTag';
 import { PrismaTypes } from '@/src/lib/prisma';
+import ClientWrapper from './components/layout/ClientWrapper';
 
 interface Props {
   searchParams: { [key: string]: string }
 }
 
 export default async function Home({ searchParams = {} }: Props) {
-  console.log('searchParams', searchParams);
   const { tag = '' } = searchParams;
     
   try {
@@ -21,10 +21,14 @@ export default async function Home({ searchParams = {} }: Props) {
     if (!blogPosts || !allTags) return <></>;
 
     return (
-      <>
+      <ClientWrapper initialTag={tag}>
         <div className="w-full flex flex-row gap-2 flex-wrap mb-6">
-          {allTags.map(tag => (
-            <SearchableTag key={tag.id} tag={tag} />
+          {allTags.map(tagOption => (
+            <SearchableTag 
+              key={tagOption.id} 
+              tag={tagOption} 
+              currentTagName={tag} 
+            />
           ))}
         </div>
 
@@ -39,7 +43,7 @@ export default async function Home({ searchParams = {} }: Props) {
             <BlogListItem blogPost={props} />
           </div>
         ))}
-      </>
+      </ClientWrapper>
     );
   } catch (error: unknown) {
     const errorMessage = error instanceof Error ? error.message : 'Unknown error'
