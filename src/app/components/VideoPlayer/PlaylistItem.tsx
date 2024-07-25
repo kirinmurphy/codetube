12a942fab78cbc66e01cbd0133b78aaa140b9724
KIRIN MURPHY <kirinmurphy@gmail.com>
2024-07-25 10:51:34 -0500

    playlist item hover control

--- src/app/components/VideoPlayer/PlaylistItem.tsx
+++ src/app/components/VideoPlayer/PlaylistItem.tsx
@@ -1,4 +1,4 @@
-import { FaMinusCircle } from "react-icons/fa";
+import { FaMinusCircle, FaPause, FaPlay } from "react-icons/fa";
 import { VideoItem } from "./types";
 import { useVideoPlayer } from "./utils/useVideoPlayer";
 import clsx from "clsx";
@@ -20,10 +20,12 @@ export function PlaylistItem({ video }: PlaylistItemProps) {
   } = useVideoPlayer();
 
   const isActiveVideo = activeVideo?.youtubeId === video.youtubeId;
+  const isActiveVideoAndPlaying = isActiveVideo && isPlaying;
+  const isActiveVideoAndNotPlaying = isActiveVideo && !isPlaying;
 
   const handlePlaylistItemSelect = () => {
-    if ( isActiveVideo && isPlaying ) { pauseVideo(); }
-    else if ( isActiveVideo && !isPlaying ) { playActiveVideo(); }
+    if ( isActiveVideoAndPlaying ) { pauseVideo(); }
+    else if ( isActiveVideoAndNotPlaying ) { playActiveVideo(); }
     else { playNewVideo({ video }); }
   }
 
@@ -31,16 +33,23 @@ export function PlaylistItem({ video }: PlaylistItemProps) {
     removeVideo(youtubeId);
   }
 
+  const Icon = isActiveVideoAndPlaying ? FaPause : FaPlay;
+
   return (
     <div className={clsx('flex border border-gray-500', {
       'font-bold bg-gray-200 text-gray-800': isActiveVideo
     })}>
       <div 
-        className={clsx('flex-grow px-4 py-2 text-lg', {
-          'cursor-pointer hover:text-white': !isActiveVideo
+        className={clsx('group flex-grow flex items-center gap-2 px-4 py-2 text-lg cursor-pointer', {
+          ' hover:text-white': !isActiveVideo
         })}
         onClick={handlePlaylistItemSelect}
       >
+        <span className={clsx("w-[25px]", {
+          'text-gray-700 group-hover:text-white': !isActiveVideo,
+          'text-gray-500 group-hover:text-black': isActiveVideo
+        })}><Icon/></span> 
+
         {title}
       </div>
       
