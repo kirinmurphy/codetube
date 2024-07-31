fb992de160112203ca9115659c9ba14216aac2b2
codethings <kirinmurphy@gmail.com>
2024-07-31 11:01:27 -0500

    adding PIP to video players (#11)

--- src/app/components/blog/BlogListItem/InlineVideoPlayer.tsx
+++ src/app/components/blog/BlogListItem/InlineVideoPlayer.tsx
@@ -4,36 +4,62 @@ import { YouTubePlayer } from "react-youtube";
 import { YoutubePlayer } from "../../widgets/YoutubePlayer";
 import { useVideoPlayerRef } from "../../VideoPlayer/utils/useClearRef";
 import { useVideoPlayer } from "../../VideoPlayer/utils/useVideoPlayer";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 interface Props {
   youtubeId: string;
 }
 
+interface InlineVideoPlayerState {
+  isPlaying: boolean;
+}
+
 export function InlineVideoPlayer ({ youtubeId }: Props) {
-  const { isPlaying } = useVideoPlayer();
+  const { 
+    isPlaying: isGlobalVideoPlayerPlaying,
+    pauseVideo: pauseGlobalVideoPlayer, 
+  } = useVideoPlayer();
+
   const inlineVideoPlayerRef = useVideoPlayerRef();
+
+  const [inlinePlayerState, setInlinePlayerState] = useState <InlineVideoPlayerState>({
+    isPlaying: false,
+  });
+
+  useEffect(() => {
+    const shouldPauseVideo = isGlobalVideoPlayerPlaying && inlineVideoPlayerRef.current;
+    if ( shouldPauseVideo ) { inlineVideoPlayerRef.current.pauseVideo(); }
+  }, [isGlobalVideoPlayerPlaying, inlineVideoPlayerRef]);
+
 
   const onReady = (event: { target: YouTubePlayer }) => {
     inlineVideoPlayerRef.current = event.target;
-  }
+  };
 
-  useEffect(() => {
-    const shouldPauseVideo = isPlaying && inlineVideoPlayerRef.current;
-    if ( shouldPauseVideo ) { inlineVideoPlayerRef.current.pauseVideo(); }
-  }, [isPlaying, inlineVideoPlayerRef]);
+  const handlePlay = () => {
+    pauseGlobalVideoPlayer();
+    setInlinePlayerState({ isPlaying: true });
+  };
+
+  const handlePause = () => {
+    setInlinePlayerState({ isPlaying: false }); 
+  };
 
   const handleVideoError = () => {
     console.error('error');
-  }
- 
+  };
+
   return (
     <div className="w-full">
       <YoutubePlayer
         videoId={youtubeId}
         onReady={onReady}
+        onPlay={handlePlay}
+        onPause={handlePause}
         onError={handleVideoError}                  
         opts={{ playerVars: { autoplay: false }}}
+        isPlaying={inlinePlayerState.isPlaying}
+        playerRef={inlineVideoPlayerRef} 
       />
     </div>
   );
--- src/app/components/widgets/YoutubePlayer.tsx
+++ src/app/components/widgets/YoutubePlayer.tsx
@@ -1,6 +1,18 @@
-import YouTube, { YouTubeProps } from "react-youtube";
+"use client";
 
-export function YoutubePlayer (playerProps: YouTubeProps) {
+import { MutableRefObject, useEffect } from "react";
+import YouTube, { YouTubePlayer, YouTubeProps } from "react-youtube";
+
+interface Props extends YouTubeProps {
+  playerRef: MutableRefObject<YouTubePlayer>;
+  isPlaying: boolean;
+}
+
+export function YoutubePlayer (props: Props) {
+  const { playerRef, isPlaying, ...playerProps } = props;
+
+  usePictureInPicture({ playerRef, isPlaying });
+
   return (
     <div className="relative pb-[56.25%] h-0 overflow-hidden">
       <YouTube
@@ -10,4 +22,34 @@ export function YoutubePlayer (playerProps: YouTubeProps) {
       />
     </div>
   );
+}
+
+interface UsePictureInPictureProps {
+  playerRef: MutableRefObject<YouTubePlayer>;
+  isPlaying: boolean;
+}
+
+function usePictureInPicture ({ playerRef, isPlaying }: UsePictureInPictureProps) {
+  useEffect(() => {
+    const enablePiP = () => {
+      console.log('enabilin!');
+
+      if (document.pictureInPictureEnabled && playerRef.current && isPlaying) {
+        const iframe = playerRef.current.getIframe();
+        iframe.requestPictureInPicture().catch((error: Error) => {
+          console.error('Failed to enter Picture-in-Picture mode:', error);
+        });
+      }
+    };
+
+    const handleVisibilityChange = () => {
+      if ( document.hidden && isPlaying ) { enablePiP(); }
+    };
+ 
+    document.addEventListener('visibilitychange', handleVisibilityChange);
+
+    return () => {
+      document.removeEventListener('visibilitychange', handleVisibilityChange);
+    };
+  }, [isPlaying, playerRef]);
 }
